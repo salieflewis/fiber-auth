58f3aff9a90809e2c8c8d85cf909bfbdb8469fd9
Salief <salieflewis@gmail.com>
2023-08-17 16:02:34 -0400

    add repo to qr code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { TorusCanvas } from '../components/TorusCanvas';
+import { QrCode } from '../components/QrCode';
 
 export default function Page() {
-  return <TorusCanvas />;
+  return (
+    <>
+      <TorusCanvas />
+      <div className='absolute hidden md:inline md:bottom-[48px] md:left-[48px]'>
+        <QrCode />
+      </div>
+    </>
+  );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,16 +3,20 @@
 import { ConnectKitProvider } from 'connectkit'
 import * as React from 'react'
 import { WagmiConfig } from 'wagmi'
-import { config } from '../wagmi'
+import { config, ethereumClient, walletConnectID } from '../wagmi'
+import { Web3Modal } from '@web3modal/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = React.useState(false)
   React.useEffect(() => setMounted(true), [])
   return (
+    <>
     <WagmiConfig config={config}>
       <ConnectKitProvider theme='minimal'>
         {mounted && children}
       </ConnectKitProvider>
     </WagmiConfig>
+    <Web3Modal projectId={walletConnectID as string} ethereumClient={ethereumClient} />
+    </>
   )
 }
